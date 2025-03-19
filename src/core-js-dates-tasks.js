41340da85e9fbe9925c3782c47b5562636035e53
Evgeniy <e.valerevich@gmail.com>
2025-03-19 12:45:56 +0300

    feat: implement 7 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -112,8 +112,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const msDiff = Date.parse(dateEnd) - Date.parse(dateStart);
+  const includeFirstDay = 1;
+  return msDiff / 1000 / 60 / 60 / 24 + includeFirstDay;
 }
 
 /**
@@ -133,10 +135,12 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
-}
+function isDateInPeriod(date, period) {
+  const dateStart = Date.parse(new Date(period.start));
+  const dateEnd = Date.parse(new Date(period.end));
 
+  return Date.parse(date) >= dateStart && Date.parse(date) <= dateEnd;
+}
 /**
  * Returns the date formatted in 'M/D/YYYY, hh:mm:ss a'.
  *
